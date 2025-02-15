cb2e6088a433b186f05b5d5f51a895a5987ee2c8
niccolot <nico_tiezzi98hotmail.it>
2025-02-15 12:27:49 -0600

    profile cmake

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ while [[ "$#" -gt 0 ]]; do
     case $1 in
         -debug|-d) build_type="debug" ;;
         -release|-r) build_type="release" ;;
+        -profile|-p) build_type="profile" ;;
         -run) run_after_build=true ;;
         *) echo "Unknown option: $1, usage: ./build.sh [d(ebug)|r(elease)]"  exit 1 ;;
     esac
@@ -24,6 +25,9 @@ if [ "$build_type" == "debug" ]; then
 elif [ "$build_type" == "release" ]; then
     cmake -DCMAKE_BUILD_TYPE=Release .
     cmake --build . 
+elif [ "$build_type" == "profile" ]; then
+    cmake -DCMAKE_BUILD_TYPE=Profile .
+    cmake --build .     
 fi
 
 if [ $? -eq 0 ]; then
