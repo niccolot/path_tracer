ff4750958a1fa408b68391a95dd17d7a0bf01313
niccolot <nico_tiezzi98hotmail.it>
2025-02-17 14:03:56 -0600

    change

--- include/utils.h
+++ include/utils.h
@@ -6,24 +6,22 @@
 #include "constants.h"
 
 inline double degs_to_rads(double degs) { return degs * pi / 180.0; }
-double random_double(double min = 0., double max = 1.);
-int random_int(int min, int max);
 
-//double random_double(double min = 0, double max = 1) {
-//    /**
-//     * @brief returns a double in the range [min, max)
-//     */
-//    static std::uniform_real_distribution<double> distribution(min, max);
-//    static std::mt19937 generator;
-//
-//    return distribution(generator);
-//}
-//
-//int random_int(int min, int max) {
-//    /**
-//     * @brief returns an int in the range [min, max]
-//     */
-//
-//    return int(random_double(min, max+1));
-//}
+inline double random_double(double min = 0, double max = 1) {
+    /**
+     * @brief returns a double in the range [min, max)
+     */
+    static std::uniform_real_distribution<double> distribution(min, max);
+    static std::mt19937 generator;
+
+    return distribution(generator);
+}
+
+inline int random_int(int min, int max) {
+    /**
+     * @brief returns an int in the range [min, max]
+     */
+
+    return int(random_double(min, max+1));
+}
 #endif
