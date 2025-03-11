54b73d2011e088b14a6f98047bd26ab011b46d7b
niccolot <nico_tiezzi98hotmail.it>
2025-03-11 14:54:16 -0500

    sdl

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include<iostream>
+#include "SDL.h"
 
 #include "camera.h"
 
 int main() {
-    Camera cam(480, 16.f / 9.f);
-    cam.set_background(Color(0.5f, 0.7f, 1.f));
-    cam.render();
+    SDL_Log("%s", "Hello world");
 }
