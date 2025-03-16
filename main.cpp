ee9ffef61e00a3603d1436a886d76b3927e92b07
niccolot <nico_tiezzi98hotmail.it>
2025-03-16 15:41:32 -0500

    update

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     try {
         App app(1920, 1080);
         app.run();
-    } catch (const SDL_Exception& e) {
-        std::cerr << e.what() << "\n";
+    } catch (const SDL_Exception& ex) {
+        std::cerr << ex.what() << "\n";
     }
 }
